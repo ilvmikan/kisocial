6f4831fbf6665533a5728d53f8c6d05a8dde6250
ilvmikan <mattdias30@gmail.com>
2024-02-15 20:46:37 -0300

    adding some modules

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object('config')
 
-from app.controllers import default
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app.controllers import index
