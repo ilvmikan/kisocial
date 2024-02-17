6d11b618ccbf5a419cd25359b352c4145b7500a1
ilvmikan <mattdias30@gmail.com>
2024-02-17 20:37:53 -0300

    edit_profile, login_require and forms

--- app/controllers/index.py
+++ app/controllers/index.py
@@ -1,7 +1,11 @@
 from flask import render_template, request, flash, redirect, url_for
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, current_user, login_required
 from app import app, db, login_manager
 from app.models.tables import User
+import os
+from .forms import *
+from werkzeug.utils import secure_filename
+
 
 @login_manager.user_loader
 def load_user(id):
@@ -15,20 +19,22 @@ def index():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
 
         user = User.query.filter_by(username=username).first()
 
         if user and user.password == password:
             login_user(user)
-            flash('logando')
+            flash('Login bem-sucedido', 'success')
             return redirect(url_for('pagina_inicial'))
         else:
-            flash('login invalido')
-            
-    return render_template('login.html')
+            flash('Login inválido. Verifique suas credenciais.', 'error')
+
+    return render_template('login.html', form=form)
 
 @app.route('/logout')
 def logout():
@@ -40,41 +46,79 @@ def logout():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    form = RegistrationForm()
     msg = ""
 
-    if request.method == 'POST':
-        # db data
-        name = request.form.get('name')
-        username = request.form.get('username')
-        email = request.form.get('email')
-        password = request.form.get('password')
-
-        # check
-        exist_username = User.query.filter_by(username=username).first()
-        exist_email = User.query.filter_by(email=email).first()
+    if form.validate_on_submit():
+        exist_username = User.query.filter_by(username=form.username.data).first()
+        exist_email = User.query.filter_by(email=form.email.data).first()
 
         if exist_username or exist_email:
             msg = "Username or email already exists. Please choose a different one."
         else:
-            # new user
-            user = User(name=name, username=username, email=email, password=password)
+            user = User(name=form.name.data, username=form.username.data, email=form.email.data, password=form.password.data)
             db.session.add(user)
             db.session.commit()
-            return render_template('pagina_inicial.html')
+            flash('Registration successful. Please log in.', 'success')
+            return redirect(url_for('login'))
 
-    return render_template('register.html', msg=msg)
+    return render_template('register.html', form=form, msg=msg)
 
 
 
 @app.route("/pagina_inicial")
+@login_required
 def pagina_inicial():
     return render_template('pagina_inicial.html')
 
 
 
 @app.route("/profile")
+@login_required
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html', user=current_user)
+
+
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}  
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/editar_perfil', methods=['GET', 'POST'])
+@login_required
+def editar_perfil():
+    form = EditProfileForm()
+
+    if form.validate_on_submit():
+        if form.username.data:
+            current_user.username = form.username.data
+
+        if form.email.data:
+            current_user.name = form.name.data
+
+        if form.email.data:
+            current_user.email = form.email.data
+
+        if form.foto_perfil.data:
+            foto_perfil = form.foto_perfil.data
+            if foto_perfil.filename:
+                if current_user.profile_picture:
+                    foto_anterior_path = os.path.join(app.config['UPLOAD_FOLDER'], current_user.profile_picture)
+                    if os.path.exists(foto_anterior_path):
+                        os.remove(foto_anterior_path)
+
+                if allowed_file(foto_perfil.filename):
+                    filename = secure_filename(foto_perfil.filename)
+                    foto_perfil.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    current_user.profile_picture = filename
+                else:
+                    flash('Tipo de arquivo não permitido para a foto do perfil', 'danger')
+
+        db.session.commit()
+        flash('Perfil atualizado com sucesso!', 'success')
+        return redirect(url_for('profile'))
+
+    return render_template('editar_perfil.html', form=form, user=current_user)
 
 
 
