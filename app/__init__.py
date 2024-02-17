c219f35bf7d887648ded7d70d6bf2ba06a8bddba
ilvmikan <mattdias30@gmail.com>
2024-02-17 11:27:39 -0300

    add login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
-from app.models import tables
+from app.models.tables import User  # Import your User model
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 from app.controllers import index
