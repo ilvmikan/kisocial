42bc9f8074d6a457d7d8c05b3b57a6681994d30c
ilvmikan <mattdias30@gmail.com>
2024-02-17 20:33:35 -0300

    pfp path database

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.config['UPLOAD_FOLDER'] = os.path.abspath(os.path.join(app.root_path, 'static', 'profile_pictures/users'))
+
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
