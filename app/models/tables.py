d5cf74a1fb71da50112bab26cb7769928c681287
ilvmikan <mattdias30@gmail.com>
2024-02-17 20:36:09 -0300

    pfp column

--- app/models/tables.py
+++ app/models/tables.py
@@ -4,17 +4,19 @@ class User(db.Model):
 
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(120), unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
     password = db.Column(db.String)
     name = db.Column(db.String)
-    email = db.Column(db.String, unique = True)
+    email = db.Column(db.String, unique=True)
+    profile_picture = db.Column(db.String(255))
 
-    def __init__(self, username, password, name, email):
+    def __init__(self, username, password, name, email, profile_picture=None):
         self.username = username
         self.password = password
         self.name = name
         self.email = email
+        self.profile_picture = profile_picture 
 
     @property
     def is_authenticated(self):
@@ -51,16 +53,12 @@ class Post(db.Model):
     def __repr__(self):
         return "<Post %r>" % self.id
     
-class Follow(db.Model):
-    # def __init__(self):
-    #     self.
-    #     self.
-
-    __tablename__ = "follow"
+class Friends(db.Model):
+    __tablename__ = "friends"
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    friend_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     user = db.relationship('User', foreign_keys=user_id)
-    follower = db.relationship('User', foreign_keys=follower_id)
+    friend = db.relationship('User', foreign_keys=friend_id)
