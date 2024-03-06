bd601259434b873273a0becaa79cc6dbf556a57e
ilvmikan <mattdias30@gmail.com>
2024-03-06 10:47:48 -0300

    new route admin

---
+++ app/controllers/admin.py
@@ -0,0 +1,33 @@
+from app import app, db
+from app.models.tables import User, Post
+
+@app.route("/delete_all_users", methods=['GET'])
+def delete_all_users():
+    try:
+        db.session.query(User).delete()
+        db.session.commit()
+
+        return "All user data deleted successfully."
+    except Exception as e:
+        db.session.rollback()
+        return f"Error deleting user data: {str(e)}"
+
+@app.route("/delete_all_posts", methods=['GET'])
+def delete_all_posts():
+    try:
+        db.session.query(Post).delete()
+        db.session.commit()
+
+        return "All Posts data deleted successfully."
+    except Exception as e:
+        db.session.rollback()
+        return f"Error deleting user data: {str(e)}"
+    
+@app.route("/show_all_users", methods=['GET'])
+def show_all_users():
+    try:
+        users = User.query.all()
+        user_data = "<hr>".join([f"Username: {user.username}<br>Senha: {user.password}<br>" for user in users])
+        return f"{user_data}"
+    except Exception as e:
+        return f"Error retrieving user data: {str(e)}"
